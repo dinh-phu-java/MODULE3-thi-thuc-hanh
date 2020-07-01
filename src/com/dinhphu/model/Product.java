6e42b33d3535909387139916b49ab2aeb1e199a9
Dinh Phu <ndp17hp@gmail.com>
2020-07-01 09:21:57 +0700

    ket noi database thanh cong

--- src/com/dinhphu/model/Product.java
+++ src/com/dinhphu/model/Product.java
@@ -1,4 +1,68 @@
 package com.dinhphu.model;
 
 public class Product {
+    int product_id;
+    String product_name;
+    double price;
+    int quantity;
+    String color;
+    int category;
+
+    public Product(int product_id, String product_name, double price, int quantity, String color, int category) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.price = price;
+        this.quantity = quantity;
+        this.color = color;
+        this.category = category;
+    }
+
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
 }
---
+++ src/com/dinhphu/services/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package com.dinhphu.services;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private final String url="jdbc:mysql://localhost:3306/thi_module_3";
+    private final String host="root";
+    private final String password="qazWSX1@";
+
+    public Connection getConnection(){
+        Connection connection=null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection= DriverManager.getConnection(url,host,password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+   
+}
